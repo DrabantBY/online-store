220890990287129fb216ef7191cb75a74c9db4ba
DrabantBY <drabant2022@gmail.com>
2022-12-24 11:47:19 +0300

    feat: add local storage for the cart and his handler

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,17 @@
-import { getGoodsCart } from '../../utils/getGoodsCart';
-
 import { Link } from 'react-router-dom';
+import type { Cart } from '../../types';
 import styles from './styles.module.scss';
+import useLocalStorageState from 'use-local-storage-state';
+import { getFromCart } from '../../helpers/handleCart';
+
 export const Header = () => {
-  const { total, amount } = getGoodsCart();
+  const [cartState] = useLocalStorageState('cart');
+  const { total, amount } = getFromCart(cartState as Cart[]);
 
   return (
     <div className={styles.header}>
       <div className={styles.header__container}>
+        <Link to={'/'}>go home</Link>
         <span>total:&nbsp;{total}$</span>
         <Link to={'cart'}> CartSVG</Link>
         <span>{amount}</span>
