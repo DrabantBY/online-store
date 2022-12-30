dd78e9a2f152175bb982de2a58f3cbb1f6c51b52
DrabantBY <drabant2022@gmail.com>
2022-12-30 12:06:30 +0300

    feat: add error page

--- src/components/Article/index.tsx
+++ src/components/Article/index.tsx
@@ -1,6 +1,6 @@
-import { useParams, Link } from 'react-router-dom';
+import { useParams, Link, useNavigate } from 'react-router-dom';
 import goods from '../../goods.json';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Product, CartItem } from '../../types';
 import useLocalStorageState from 'use-local-storage-state';
 import { addToCart, removeFromCart, isInCart } from '../../helpers/handleCart';
@@ -9,6 +9,14 @@ import './article.scss';
 export const Article = (props: { elemId?: number; elemName?: string }) => {
   const { elemId, elemName } = props;
   const { id } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (id && (isNaN(+id) || +id <= 0 || +id > goods.length)) {
+      navigate('/error');
+    }
+  }, [id, navigate]);
+
   let resID: number;
   if (elemId) {
     resID = elemId;
@@ -21,106 +29,109 @@ export const Article = (props: { elemId?: number; elemName?: string }) => {
 
   const article = goods.find((article) => article.id === resID);
   const [indexImg, setIndexImg] = useState(0);
-  const { brand, title, category, description, price, rating, discountPercentage, images, stock }: Product = article!;
-  return (
-    <div className="article-container">
-      <div className="article-container-path-image">
-        {!elemName ? (
-          <div className="article-container-path">
-            <Link to={`/`}>
-              <span>Store</span>
-            </Link>
-            <span>{`>>`}</span>
-            <span>{`${category}`}</span>
-            <span>{`>>`}</span>
-            <span>{`${brand}`}</span>
-            <span>{`>>`}</span>
-            <span>{`${title}`}</span>
-          </div>
-        ) : null}
-        <div className="article-container-image">
+
+  if (article) {
+    const { brand, title, category, description, price, rating, discountPercentage, images, stock }: Product = article;
+    return (
+      <div className="article-container">
+        <div className="article-container-path-image">
           {!elemName ? (
-            <ul className="article-image-list">
-              {Array.from(new Set(images)).map((element, index, array) => (
-                <li
-                  className="article-list-elem"
-                  key={`article_img${index}`}
-                  onClick={() => {
-                    setIndexImg(index);
-                  }}>
-                  <img className="article-image-list-elem" src={element} alt="article" />
-                </li>
-              ))}
-            </ul>
-          ) : null}
-          <div className="article-container-main-image">
-            <div className="article-container-title">
-              <span className="article-title">{title}</span>
-              <span className="article-brand">{`by ${brand}`}</span>
-            </div>
-            {elemName ? (
-              <Link to={`${resID}`}>
-                <img className="article-main-image" src={images[indexImg]} alt="" />
-              </Link>
-            ) : (
-              <img className="article-main-image" src={images[indexImg]} alt="" />
-            )}
-          </div>
-        </div>
-      </div>
-      <div className="article-container-text">
-        <div className="article-container-price">
-          <span className="article-price-new">{`Price: ${(+price - +price * (+discountPercentage / 100)).toFixed(
-            2
-          )}`}</span>
-          <p className="article-price-old">{price}</p>
-          <p className="article-price-discount">{`-${discountPercentage}%`}</p>
-        </div>
-        <div className="article-container-stock-rating">
-          <p>
-            <span className="article-stock">Remaining in stock: </span>
-            <b>{stock}</b>
-          </p>
-          <p>
-            <span className="article-rating">Product rating: </span>
-            <b>{rating}</b>
-          </p>
-        </div>
-        <p className="article-description">{description}</p>
-        <div className="article-container-button">
-          <button
-            className={isInCart(cartState, resID) ? 'article-list-active' : 'article-list-default'}
-            type="button"
-            onClick={() => {
-              const data = { id: resID, amount: 1 };
-              const newCartState = isInCart(cartState, resID)
-                ? removeFromCart(cartState, resID)
-                : addToCart(cartState, data);
-              setCartState(newCartState);
-            }}>
-            {isInCart(cartState, resID) ? <span>Remove from Cart</span> : <span>Add to Cart</span>}
-          </button>
-          {!elemName ? (
-            <button className="article-button-buy">
-              <span>buy now</span>
-            </button>
-          ) : null}
-          {!elemName ? (
-            <button className="article-button-back">
+            <div className="article-container-path">
               <Link to={`/`}>
-                <span>back</span>
+                <span>Store</span>
               </Link>
-            </button>
+              <span>{`>>`}</span>
+              <span>{`${category}`}</span>
+              <span>{`>>`}</span>
+              <span>{`${brand}`}</span>
+              <span>{`>>`}</span>
+              <span>{`${title}`}</span>
+            </div>
           ) : null}
-          {elemName ? (
-            <button className="article-button-back">
-              <Link to={`${resID}`}>
-                <span>Details</span>
-              </Link>
+          <div className="article-container-image">
+            {!elemName ? (
+              <ul className="article-image-list">
+                {Array.from(new Set(images)).map((element, index, array) => (
+                  <li
+                    className="article-list-elem"
+                    key={`article_img${index}`}
+                    onClick={() => {
+                      setIndexImg(index);
+                    }}>
+                    <img className="article-image-list-elem" src={element} alt="article" />
+                  </li>
+                ))}
+              </ul>
+            ) : null}
+            <div className="article-container-main-image">
+              <div className="article-container-title">
+                <span className="article-title">{title}</span>
+                <span className="article-brand">{`by ${brand}`}</span>
+              </div>
+              {elemName ? (
+                <Link to={`${resID}`}>
+                  <img className="article-main-image" src={images[indexImg]} alt="" />
+                </Link>
+              ) : (
+                <img className="article-main-image" src={images[indexImg]} alt="" />
+              )}
+            </div>
+          </div>
+        </div>
+        <div className="article-container-text">
+          <div className="article-container-price">
+            <span className="article-price-new">{`Price: ${(+price - +price * (+discountPercentage / 100)).toFixed(
+              2
+            )}`}</span>
+            <p className="article-price-old">{price}</p>
+            <p className="article-price-discount">{`-${discountPercentage}%`}</p>
+          </div>
+          <div className="article-container-stock-rating">
+            <p>
+              <span className="article-stock">Remaining in stock: </span>
+              <b>{stock}</b>
+            </p>
+            <p>
+              <span className="article-rating">Product rating: </span>
+              <b>{rating}</b>
+            </p>
+          </div>
+          <p className="article-description">{description}</p>
+          <div className="article-container-button">
+            <button
+              className={isInCart(cartState, resID) ? 'article-list-active' : 'article-list-default'}
+              type="button"
+              onClick={() => {
+                const data = { id: resID, amount: 1 };
+                const newCartState = isInCart(cartState, resID)
+                  ? removeFromCart(cartState, resID)
+                  : addToCart(cartState, data);
+                setCartState(newCartState);
+              }}>
+              {isInCart(cartState, resID) ? <span>Remove from Cart</span> : <span>Add to Cart</span>}
             </button>
-          ) : null}
+            {!elemName ? (
+              <button className="article-button-buy">
+                <span>buy now</span>
+              </button>
+            ) : null}
+            {!elemName ? (
+              <button className="article-button-back">
+                <Link to={`/`}>
+                  <span>back</span>
+                </Link>
+              </button>
+            ) : null}
+            {elemName ? (
+              <button className="article-button-back">
+                <Link to={`${resID}`}>
+                  <span>Details</span>
+                </Link>
+              </button>
+            ) : null}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else return null;
 };
